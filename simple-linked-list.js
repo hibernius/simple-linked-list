55eaa56ce166c45416bc785f4f747e229f253639
= <korova.cow@gmail.com>
2023-05-17 13:02:30 +0100

    Add JSDoc

--- simple-linked-list.js
+++ simple-linked-list.js
@@ -1,39 +1,88 @@
-
+/**
+ * Class representing a linked list element.
+ */
 export class Element {
 
+  /**
+   * @property value
+   * @type {*}
+   * @private
+   */
   #value;
-  
+
+  /**
+   * @property next
+   * @type {Element|null}
+   * @private
+   */
   #next = null;
 
+  /**
+   * Constructor.
+   * @param {*} value
+   */
   constructor(value) {
     this.#value = value;
   }
 
+  /**
+   * Get the value.
+   * @returns {*}
+   */
   get value() {
     return this.#value;
   }
 
+  /**
+   * Get the next Element.
+   * @returns {Element}
+   */
   get next() {
     return this.#next;
   }
 
+  /**
+   * Set the next Element.
+   * @param {*} value
+   */
   set next(value) {
     this.#next = value;
   }
 }
 
+/**
+ * Class representing a linked list.
+ */
 export class List {
 
+  /**
+   * @property length
+   * @type {number}
+   * @private
+   */
   #length = 0;
 
+  /**
+   * @property head
+   * @type {Element|null}
+   * @private
+   */
   #head = null;
 
+  /**
+   * Constructor.
+   * @param {*[]} values
+   */
   constructor(values) {
     if (Array.isArray(values)) {
       this.addFromArray(values);
     }
   }
 
+  /**
+   * Add an Element to the list.
+   * @param {Element} nextValue
+   */
   add(nextValue) {
     if (this.#head === null) {
       this.#head = nextValue;
@@ -44,20 +93,36 @@ export class List {
     this.#length++;
   }
 
+  /**
+   * Create and add Elements based on array values.
+   * @param {*[]} values
+   */
   addFromArray(values) {
     for (let i=0; i<values.length; i++) {
       this.add(new Element(values[i]));
     }
   }
 
+  /**
+   * Get current list length.
+   * @returns {number}
+   */
   get length() {
     return this.#length;
   }
 
+  /**
+   * Get current head.
+   * @returns {Element|null}
+   */
   get head() {
     return this.#head;
   }
 
+  /**
+   * Create and return an array of list values.
+   * @returns {*[]}
+   */
   toArray() {
     let output = [];
     let currentElement = this.#head;
@@ -69,6 +134,10 @@ export class List {
     return output;
   }
 
+  /**
+   * Reverse the list and return it.
+   * @returns {List}
+   */
   reverse() {
     let previousElement = null;
     let currentElement = this.#head;
